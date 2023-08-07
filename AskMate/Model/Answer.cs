488f2f92612d39c292c42aa45199c1af3905c50a
Máté Szakasits <120025444+mate153@users.noreply.github.com>
2023-08-07 15:01:32 +0200

    [ UPDATE ] Fill model properties (answer, user)

--- AskMate/Model/Answer.cs
+++ AskMate/Model/Answer.cs
@@ -2,5 +2,8 @@
 
 public class Answer
 {
-    
+    public int Id { get; set; }
+    public string Message { get; set; }
+    public int Question_id { get; set; }
+    public DateTime Submission_time { get; set; }
 }
