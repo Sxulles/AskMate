feb255ac4e66c9e0c02bc4f819865437ec09256e
Sxulles <seres1322@gmail.com>
2023-08-07 18:01:22 +0200

    [ UPDATE ] QuestionRepository.cs, model connector implemented in GetById() method

--- AskMate/Model/Repositories/QuestionRepository.cs
+++ AskMate/Model/Repositories/QuestionRepository.cs
@@ -37,7 +37,7 @@ public class QuestionRepository
         _connection.Close();
     }
 
-    public (Question, List<Answer>) GetById(int id)
+    public QuestionAnswersPair? GetById(int id)
     {
         _connection.Open();
         var adapter = new NpgsqlDataAdapter("SELECT q.id AS question_id, q.title, q.description, q.submission_time AS question_submission_time, a.id AS answer_id, a.message, a.submission_time AS answer_submission_time, a.question_id FROM questions q JOIN answers a ON q.id = a.question_id WHERE q.id = :id ", _connection);
@@ -46,34 +46,36 @@ public class QuestionRepository
         var dataSet = new DataSet();
         adapter.Fill(dataSet);
         var table = dataSet.Tables[0];
-        Question question;
+        Question? question;
         List<Answer> answers = new List<Answer>();
-        (Question a, List<Answer> b) returnValue = (null, null);
         
         if (table.Rows.Count > 0)
         {
             DataRow row = table.Rows[0];
-            question = new Question()
+            question = new Question
             {
                 Id = (int)row["question_id"],
                 Title = (string)row["title"],
                 Description = (string)row["description"],
                 SubmissionTime = (DateTime)row["question_submission_time"]
             };
-            for (int i = 1; i < table.Rows.Count; i++)
+            foreach (DataRow newRow in table.Rows)
             {
                 answers.Add(new Answer()
                 {
-                    Id = (int)table.Rows[i]["answer_id"],
-                    Message = (string)table.Rows[i]["message"],
-                    QuestionId = (int)table.Rows[i]["question_id"],
-                    SubmissionTime = (DateTime)table.Rows[i]["answer_submission_time"]
+                    Id = (int)newRow["answer_id"],
+                    Message = (string)newRow["message"],
+                    QuestionId = (int)newRow["question_id"],
+                    SubmissionTime = (DateTime)newRow["answer_submission_time"]
                 });
             }
-            returnValue.b = answers;
-            returnValue.a = question;
+            return new QuestionAnswersPair
+            {
+                Question = question, 
+                Answers = answers
+            };
         }
-        return returnValue;
+        return null;
     }
     
 }
