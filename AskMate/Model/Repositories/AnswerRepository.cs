c466b486bcf3533e81921823fb15c775d74fb3eb
Sxulles <seres1322@gmail.com>
2023-08-07 15:55:15 +0200

    [ UPDATE ] AnswerRepository.cs, repository GetAll method added

--- AskMate/Model/Repositories/AnswerRepository.cs
+++ AskMate/Model/Repositories/AnswerRepository.cs
@@ -1,6 +1,37 @@
-﻿namespace AskMate.Model.Repositories;
+﻿using System.Data;
+using Npgsql;
+
+namespace AskMate.Model.Repositories;
 
 public class AnswerRepository
 {
+    private readonly NpgsqlConnection _connection;
+
+    public AnswerRepository(NpgsqlConnection connection)
+    {
+        _connection = connection;
+    }
     
+    public IEnumerable<Answer> GetAll()
+    {
+        _connection.Open();
+        var adapter = new NpgsqlDataAdapter("SELECT * FROM answers", _connection);
+
+        var dataSet = new DataSet();
+        adapter.Fill(dataSet);
+        var table = dataSet.Tables[0];
+
+        foreach (DataRow row in table.Rows)
+        {
+            yield return new Answer
+            {
+                Id = (int)row["id"],
+                Message = (string)row["title"],
+                QuestionId = (int)row["description"],
+                SubmissionTime = (DateTime)row["submission_time"]
+            };
+        }
+        
+        _connection.Close();
+    }
 }
