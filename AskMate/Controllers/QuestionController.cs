89381cbbd35d8770cf5e47b571f9095880c56060
McKay <kg89szeged@gmail.com>
2023-08-08 10:17:07 +0200

    [ UPDATE ] CreateQuestion endpoint was added and tested

--- AskMate/Controllers/QuestionController.cs
+++ AskMate/Controllers/QuestionController.cs
@@ -8,17 +8,29 @@ namespace AskMate.Controllers;
 [Route("[controller]")]
 public class QuestionController : ControllerBase
 {
+    // Get all questions
     [HttpGet]
     public IActionResult GetAll()
     {
         var repository = new QuestionRepository(new NpgsqlConnection(ConnectionData.connectionString));
         return Ok(repository.GetAll("DESC", "submission_time"));
     }
+    
+    // Get a question and the answers
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
         var repository = new QuestionRepository(new NpgsqlConnection(ConnectionData.connectionString));
         
         return Ok(repository.GetById(id));
+    }
+    
+    // Insert a new Question into database
+    [HttpPost()]
+    public IActionResult CreateQuestion(Question question)
+    {
+        var repository = new QuestionRepository(new NpgsqlConnection(ConnectionData.connectionString));
+
+        return Ok(repository.CreateQuestion(question));
     }
 }
