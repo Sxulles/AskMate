22ac05f3d64a14696d5a5225b926f16799c06ef1
Sxulles <seres1322@gmail.com>
2023-08-08 10:57:16 +0200

    [ UPDATE ] AnswerRepository.cs, CreateAnswer() method implemented, edge case handled

--- AskMate/Model/Repositories/AnswerRepository.cs
+++ AskMate/Model/Repositories/AnswerRepository.cs
@@ -34,4 +34,33 @@ public class AnswerRepository
         
         _connection.Close();
     }
+
+    public int? CreateAnswer(Answer answer)
+    {
+        _connection.Open();
+        var query = new NpgsqlDataAdapter("SELECT * FROM questions WHERE id = :question_id", _connection);
+        query.SelectCommand?.Parameters.AddWithValue(":question_id", answer.QuestionId);
+        DataTable queryResult = new DataTable();
+    
+        
+        query.Fill(queryResult);
+        int? lastInsertId = null;
+        
+        
+        if (queryResult.Rows.Count > 0)
+        {
+            var adapter = new NpgsqlDataAdapter(
+                "INSERT INTO answers (message, question_id, submission_time) VALUES (:message, :question_id, :submission_time) RETURNING id",
+                _connection
+            );
+            adapter.SelectCommand?.Parameters.AddWithValue(":message", answer.Message);
+            adapter.SelectCommand?.Parameters.AddWithValue(":question_id", answer.QuestionId);
+            adapter.SelectCommand?.Parameters.AddWithValue(":submission_time", DateTime.Now);
+
+            lastInsertId = (int)adapter.SelectCommand?.ExecuteScalar();
+        }
+        _connection.Close();
+        
+        return lastInsertId;
+    }
 }
