4e04ba2e3ced237234230ceb1b9499854fd5ecff
McKay <kg89szeged@gmail.com>
2023-08-08 11:32:35 +0200

    [ UPDATE ] CreateUser API endpoint was added

--- AskMate/Controllers/UserController.cs
+++ AskMate/Controllers/UserController.cs
@@ -1,9 +1,20 @@
-﻿namespace AskMate.Controllers;
+﻿using AskMate.Model;
+using AskMate.Model.Repositories;
+using Npgsql;
+
+namespace AskMate.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    
+    // Insert a new user to the database
+    [HttpPost()]
+    public IActionResult CreateUser(User user)
+    {
+        var repository = new UserRepository(new NpgsqlConnection(ConnectionData.connectionString));
+
+        return Ok(repository.CreateUser(user));
+    }
 }
