66f4bf51c812004cbd95fa905f8ae2a13885efde
Sxulles <seres1322@gmail.com>
2023-08-08 13:33:13 +0200

    [ DELETE ] GetAll() db operation removed

--- AskMate/Model/Repositories/AnswerRepository.cs
+++ AskMate/Model/Repositories/AnswerRepository.cs
@@ -11,29 +11,6 @@ public class AnswerRepository
     {
         _connection = connection;
     }
-    
-    public IEnumerable<Answer> GetAll()
-    {
-        _connection.Open();
-        var adapter = new NpgsqlDataAdapter("SELECT * FROM answers", _connection);
-
-        var dataSet = new DataSet();
-        adapter.Fill(dataSet);
-        var table = dataSet.Tables[0];
-
-        foreach (DataRow row in table.Rows)
-        {
-            yield return new Answer
-            {
-                Id = (int)row["id"],
-                Message = (string)row["title"],
-                QuestionId = (int)row["description"],
-                SubmissionTime = (DateTime)row["submission_time"]
-            };
-        }
-        
-        _connection.Close();
-    }
 
     public int? CreateAnswer(Answer answer)
     {
