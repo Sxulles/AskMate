5df0ea4d17f08bf71f7aa75a397ae9c821315d04
McKay <kg89szeged@gmail.com>
2023-08-08 13:31:27 +0200

    [ REFACT ] CreateUser method now better

--- AskMate/Model/Repositories/UserRepository.cs
+++ AskMate/Model/Repositories/UserRepository.cs
@@ -13,16 +13,16 @@ public class UserRepository
     }
     
     // Create a new User
-    public int CreateUser(User user)
+    public int CreateUser(string username, string email, string password)
     {
         _connection.Open();
         var adapter = new NpgsqlDataAdapter(
             "INSERT INTO users (username, email, password, registration_time) VALUES (:username, :email, :password, :registration_time) RETURNING id",
             _connection
         );
-        adapter.SelectCommand?.Parameters.AddWithValue(":username", user.Username);
-        adapter.SelectCommand?.Parameters.AddWithValue(":email", user.Email);
-        adapter.SelectCommand?.Parameters.AddWithValue(":password", user.Password);
+        adapter.SelectCommand?.Parameters.AddWithValue(":username", username);
+        adapter.SelectCommand?.Parameters.AddWithValue(":email", email);
+        adapter.SelectCommand?.Parameters.AddWithValue(":password", password);
         adapter.SelectCommand?.Parameters.AddWithValue(":registration_time", DateTime.Now);
 
         var lastInsertId = (int)adapter.SelectCommand?.ExecuteScalar();
